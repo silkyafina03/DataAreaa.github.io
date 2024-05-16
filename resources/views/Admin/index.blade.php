c79dd641242c7b6e84211d95d1663c6ddd5e55d2
DataAreaa <164318797+DataAreaa@users.noreply.github.com>
2024-05-16 12:22:48 +0700

    Update index.blade.php

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -1,7 +1,14 @@
 @extends('layout/aplikasi')
 @section('konten')
-    <a href="/Admin/create" class="btn btn-primary">+Tambah Data Area</a>
-    <table class="table">
+<div class="pb-3">
+    <form class="d-flex" action="{{ url('Admin') }}" method="get">
+        <input class="form-control me-1" type="search" name="katakunci" value="{{ Request::get('katakunci') }}" placeholder="Masukkan kata kunci" aria-label="Search">
+        <button class="btn btn-secondary" type="submit">Cari</button>
+    </form>
+</div>
+    <a href="/Admin/create" class="btn btn-primary">Data Area <i class="fa-solid fa-plus"></i></a>  
+    <a class="btn btn-success" href="/Admin/cetak"> Cetak Data <i class="fa-solid fa-print"></i></a>
+    <table class="table" >
         <thead>
             <tr>
                 <th> Kode Area </th>
@@ -10,7 +17,7 @@
                 <th> Wilayah </th>
                 <th> Kota </th>
                 <th> Provinsi </th>
-                <th>Aksi</th>
+                <th> Aksi </th>
             </tr>
         </thead>
         <tbody>
@@ -19,18 +26,19 @@
                     <td>{{$item->kode_area}}</td>
                     <td>{{$item->nama_area}}</td>
                     <td>{{$item->deskripsi}}</td>
-                    <td>{{$item->wilayah}}</td>
+                    <td>{{$item->wilayah->nama_wilayah}}</td>
                     <td>{{$item->kota}}</td>
                     <td>{{$item->provinsi}}</td>
                     <td> 
-                        <a class="btn btn-secondary btn-sm" href="{{url('/Admin/'.$item->kode_area)}}">Detail</a>
+                        
+                        <a class="btn btn-info btn-sm" href="{{route('detail', $item->kode_area)}}"><i class="fa-solid fa-circle-info"></i></a>
 
-                        <a class="btn btn-warning btn-sm" href="{{url('/Admin/'.$item->id.'/edit')}}">Edit</a>
+                        <a class="btn btn-warning btn-sm" href="{{url('/Admin/'.$item->kode_area.'/edit')}}"><i class="fa-solid fa-pencil"></i></a>
 
                         <form onsubmit="return confirm('Yakin untuk hapus data?')" class="d-inline" action="{{url('/Admin/'.$item->kode_area)}}" method="post">
                             @csrf
                             @method('DELETE')
-                            <button class="btn btn-danger btn-sm" type="submit">Hapus</button>
+                            <button class="btn btn-danger btn-sm" type="submit"><i class="fa-solid fa-trash"></i></button>
                         </form>
                     </td>
                 </tr>
