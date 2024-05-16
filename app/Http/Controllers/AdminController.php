0c5c513e8ed39d33f851329ddc48108dade5f792
DataAreaa <164318797+DataAreaa@users.noreply.github.com>
2024-05-16 12:19:33 +0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Admin;
+use App\Models\Wilayah;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
@@ -14,12 +15,32 @@ class AdminController extends Controller
      * 
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $data = Admin::all();
-        $data = Admin::orderBy('kode_area','asc')->paginate(5);
-        return view('Admin/index')-> with('data', $data);
+        $katakunci = $request->katakunci;
+        $jumlahbaris = 5; // or you can use a different value for pagination
+        
+        if (strlen($katakunci)) {
+            $data = Admin::where('kode_area', 'like', "%$katakunci%")
+                ->orWhere('nama_area', 'like', "%$katakunci%")
+                ->orWhere('deskripsi', 'like', "%$katakunci%")
+                ->orWhere('wilayah_id', 'like', "%$katakunci%")
+                ->orWhere('kota', 'like', "%$katakunci%")
+                ->orWhere('provinsi', 'like', "%$katakunci%")
+                ->paginate($jumlahbaris);
+        } else {
+            $data = Admin::with('wilayah')->orderBy('kode_area', 'asc')->paginate($jumlahbaris);
+        }
+ 
+        return view('Admin/index', compact('data','katakunci'))-> with('data', $data);
+   
     }
+    public function cetak()
+    {
+        $Cetak = Admin::orderBy('kode_area', 'asc')->with('wilayah')->paginate(20);
+        return view('Admin/cetak')->with('cetak', $Cetak);
+    }
+    
     /**
      * Store a newly created resource in storage.
      * 
@@ -29,22 +50,24 @@ class AdminController extends Controller
      */
     public function detail($id)
     {
-       
+        $data = Admin::where('kode_area', $id)->first();
+
+    if (!$data) {
+        return redirect('/Admin')->with('error', 'Data tidak ditemukan.');
     }
+
+    $wil = Wilayah::find($data->wilayah_id);
+
+    return view('Admin.detail', compact('wil', 'data'));
+    }
+
     public function store(Request $request)
     {
-        Session::flash('kode_area', $request->kode_area);
-        Session::flash('nama_area', $request->nama_area);
-        Session::flash('deskripsi', $request->deskripsi);
-        Session::flash('wilayah', $request->wilayah);
-        Session::flash('kota', $request->kota);
-        Session::flash('provinsi', $request->provinsi);
-
         $request->validate([
             'kode_area'=> 'required|unique:Admin,kode_area',
             'nama_area'=> 'required',
             'deskripsi'=> 'required',
-            'wilayah'=> 'required',
+            'wilayah_id'=> 'required',
             'kota'=> 'required',
             'provinsi'=> 'required'
         ],[
@@ -52,24 +75,26 @@ class AdminController extends Controller
             'kode_area.unique'=>'Kode Area Sudah Ada, Masukkan Kode Lain',
             'nama_area.required'=>'Nama Area wajib diisi',
             'deskripsi.required'=>'Deskripsi wajib diisi',
-            'wilayah.required'=>'Wilayah wajib diisi',
-            'kota.required'=>'Kota Area wajib diisi',
-            'provinsi.required'=>'Provinsi wajib diisi'
+            'kota.required'=>'Kota wajib diisi',
+            'provinsi.required'=>'Provinsi wajib diisi',
+            'wilayah_id.required'=>'Wilayah wajib dipilih',
         ]);
         $data = [
             'kode_area' => $request->input('kode_area'),
             'nama_area' => $request->input('nama_area'),
             'deskripsi' => $request->input('deskripsi'),
-            'wilayah' => $request->input('wilayah'),
+            'wilayah_id' => $request->input('wilayah_id'),
             'kota' => $request->input('kota'),
             'provinsi' => $request->input('provinsi')
         ];
         Admin::create($data);
         return redirect('Admin')->with('success','Berhasil menambahkan data area');
+
     }
     function create(Request $request)
     {
-        return view('Admin/create');
+        $wil = Wilayah::all();
+        return view('Admin/create',compact('wil'));   
     }
 
     /**
@@ -80,11 +105,16 @@ class AdminController extends Controller
      */
     public function show($id)
     {
-       $data= Admin::where('kode_area', $id)->first();
-        return view('Admin/detail')->with('data',$data);
-        
+        $data = Admin::where('kode_area', $id)->first();
+    
+        if (!$data) {
+            return redirect('/Admin')->with('error', 'Data tidak ditemukan.');
+        }
+    
+        $wil = Wilayah::find($data->wilayah_id);
+    
+        return view('Admin/detail', compact('wil', 'data'));
     }
-
     /**
      * Show the form for editing the specified resource.
      * @param  int  $id
@@ -92,8 +122,9 @@ class AdminController extends Controller
      */
     public function edit($id)
     {
-         $data = Admin::find($id);
-        return view('Admin/edit')->with('data', $data);
+        $wil = Wilayah::all();
+        $data = Admin::with('Wilayah')->findOrFail($id);
+        return view('Admin/edit', compact('wil', 'data'));
     }
 
     /**
@@ -108,25 +139,25 @@ class AdminController extends Controller
         $request->validate([
             'nama_area'=> 'required',
             'deskripsi'=> 'required',
-            'wilayah'=> 'required',
+            'wilayah_id'=> 'required',
             'kota'=> 'required',
             'provinsi'=> 'required'
         ],[
             'nama_area.required'=>'Nama Area wajib diisi',
             'deskripsi.required'=>'Deskripsi wajib diisi',
-            'wilayah.required'=>'Wilayah wajib diisi',
+            'wilayah_id.required'=>'Wilayah wajib dipilih',
             'kota.required'=>'Kota Area wajib diisi',
             'provinsi.required'=>'Provinsi wajib diisi'
         ]);
         $data=[
             'nama_area'=> $request->input('nama_area'),
-            'deksripsi'=> $request->input('deskripsi'),
-            'wilayah'=> $request->input('wilayah'),
+            'deskripsi'=> $request->input('deskripsi'),
+            'wilayah_id'=> $request->input('wilayah_id'),
             'kota'=> $request->input('kota'),
             'provinsi'=> $request->input('provinsi')
         ];
         Admin::where('kode_area',$id)->update($data);
-        return redirect('/Admin')->with('success','Berhasil melakukan update');
+        return redirect('Admin')->with('success','Berhasil melakukan update');
     }
 
     /**
