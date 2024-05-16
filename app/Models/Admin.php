b166a1bb00abf6b34221b976ecfefa0c5030469e
DataAreaa <164318797+DataAreaa@users.noreply.github.com>
2024-05-16 12:25:37 +0700

    Update Admin.php

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model
 {
     use HasFactory;
-    protected $table = "Admin";
-    protected $fillable = ['kode_area','nama_area','deskripsi','wilayah','kota','provinsi'];
+    protected $table = "admin";
+    public $incrementing = false;
+    protected $primaryKey = 'kode_area';
+    protected $fillable = ['kode_area','nama_area','deskripsi','wilayah_id','kota','provinsi'];
+
+    public function wilayah()
+    {
+        return $this->belongsTo(Wilayah::class, 'wilayah_id', 'wilayah_id');
+    }
 }
