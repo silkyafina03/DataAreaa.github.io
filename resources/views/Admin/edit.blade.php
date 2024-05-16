e23c10f9e3733923fcc6833c6f4d2c859b69317e
DataAreaa <164318797+DataAreaa@users.noreply.github.com>
2024-05-16 12:22:23 +0700

    Update edit.blade.php

--- resources/views/Admin/edit.blade.php
+++ resources/views/Admin/edit.blade.php
@@ -1,35 +1,42 @@
 @extends('layout/aplikasi')
 
 @section('konten')
-    <a href="/Admin" class="btn btn-secondary"> KEMBALI </a>
-    <form method="post" action="{{'/Admin/'.$data->id}}">
+    <form method="post" action="{{'/Admin/'.$data->kode_area}}">
         @csrf
-        @method('put')
+        @method('PUT')
        <div class="mb">
         <h1>Kode Area: {{$data->kode_area}}</h1>
        </div>
         <div class="mb-3">
             <label for="nama_area" class="form-label">Nama Area</label>
-            <input type="text" class="form-control" name="nama_area" id="nama_area" value="{{$data->nama_akses}}">
+            <input type="text" class="form-control" name="nama_area" id="nama_area" value="{{$data->nama_area}}">
         </div>
         <div class="mb-3">
             <label for="deskripsi" class="form-label">Deskripsi</label>
             <textarea class="form-control" name="deskripsi">{{$data->deskripsi}}</textarea>
         </div>
         <div class="mb-3">
-            <label for="wilayah" class="form-label">Wilayah</label>
-            <input type="text" class="form-control" name="wilayah" id="wilayah" {{$data->wilayah}}>
+            <label for="wilayah_id" class="form-label">Wilayah</label>
+            <select class="form-control select2" style="width:100%"; name="wilayah_id" id="wilayah_id">
+                <option disabled value> Pilih Wilayah </option>
+                <option value="{{$data->wilayah_id}}"> {{$data->Wilayah->nama_wilayah}}</option>
+                
+                @foreach($wil as $item)
+                <option value="{{$item->wilayah_id}}">{{$item->nama_wilayah}} </option>
+                @endforeach
+            </select>
         </div>
         <div class="mb-3">
             <label for="kota" class="form-label">Kota</label>
-            <input type="text" class="form-control" name="kota" id="kota" {{$data->kota}}>
+            <input type="text" class="form-control" name="kota" id="kota" value="{{$data->kota}}">
         </div>
         <div class="mb-3">
             <label for="provinsi" class="form-label">Provinsi</label>
-            <input type="text" class="form-control" name="provinsi" id="provinsi" {{$data->provinsi}}>
+            <input type="text" class="form-control" name="provinsi" id="provinsi" value="{{$data->provinsi}}">
         </div>
         <div class="mb-3">
-            <button type="submit" class="btn btn-primary"> UPDATE </button>
+            <button type="submit" class="btn btn-success"> UPDATE </button>
+            <a href="/Admin" class="btn btn-secondary"> KEMBALI </a>
         </div>
     </form>
 @endsection
