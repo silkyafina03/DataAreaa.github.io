f9b4c98e78c80932aada588f52b143bd838de55b
DataAreaa <164318797+DataAreaa@users.noreply.github.com>
2024-05-16 12:23:28 +0700

    Update welcome.blade.php

--- resources/views/sesi/welcome.blade.php
+++ resources/views/sesi/welcome.blade.php
@@ -1,10 +1,10 @@
 @extends('layout/aplikasi')
 
 @section('konten')
-    <div class="w-50 text-center border rounded px-3 py-3 mx-auto">
+    <div class="w-50 text-center border rounded px-3 py-4 mx-auto">
         <h1>Selamat Datang</h1>
         <p>Silahkan login atau register untuk masuk ke aplikasi</p>
-        <a href="/sesi" class="btn btn-primary btn-lg">LOGIN</a>
-        <a href="/sesi/register" class="btn btn-success btn-lg">REGISTER</a>
+        <a href="/sesi" class="btn btn-primary btn-lg">LOGIN <i class="fa-solid fa-right-to-bracket"></i></a>
+        <a href="/sesi/register" class="btn btn-success btn-lg">REGISTER <i class="fa-solid fa-arrow-right-to-bracket"></i></a>
     </div>
 @endsection
